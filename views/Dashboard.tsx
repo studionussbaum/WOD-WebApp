b04e9b46ae7695f89208db22b071438122106879
studionussbaum <m4tps5pnhz@privaterelay.appleid.com>
2025-10-28 19:57:56 +0100

    Update Dashboard.tsx

--- views/Dashboard.tsx
+++ views/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { Workout, OneRepMax, COMMON_LIFTS } from '../types';
-import { getTrainingSuggestion } from '../services/geminiService';
+import { getTrainingSuggestion } from '../services/openaiService';
 import WorkoutCard from '../components/WorkoutCard';
 
 interface DashboardProps {
